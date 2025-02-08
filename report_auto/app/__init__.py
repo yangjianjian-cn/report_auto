82da87d0f9f9894a59de56c4aab95a695732f229
yangjj <yang5.1-login@qq.com>
2025-02-08 14:00:46 +0800

    docker部署

--- report_auto/app/__init__.py
+++ report_auto/app/__init__.py
@@ -10,21 +10,6 @@ env_output_path = os.getenv('output_path')
 env_template_path = os.getenv('template_path')
 
 from tools.utils.ConnectionUtils import DatabasePool
-
-# jdbc_mysql=10.0.4.14:ba:3306:1qazxsw2:measurement
-# jdbc_mysql=sh-cynosdbmysql-grp-rykty3lm.sql.tencentcdb.com:ba:26338:1qazxsw2:measurement
-
-# 创建全局数据库连接池实例
-# jdbc_mysql: str = os.getenv('jdbc_mysql')
-# jdbc_mysql_arr = jdbc_mysql.split(":")
-# mysql_config = {
-#     'host': jdbc_mysql_arr[0],
-#     'user': jdbc_mysql_arr[1],
-#     'port': int(jdbc_mysql_arr[2]),
-#     'password': jdbc_mysql_arr[3],
-#     'database': jdbc_mysql_arr[4],
-#     'charset': 'utf8mb4'
-# }
 mysql_config = {
     'host': os.getenv('MYSQL_HOST'),
     'user': os.getenv('MYSQL_USER'),
